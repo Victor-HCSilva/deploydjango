c71bfaa0bc607b491b62de8848312cb14bb1bbc9
Victor Hugo <victorh.csilva20@gmail.com>
2025-01-16 07:04:03 -0300

    melhorias

---
+++ portal_do_paciente/const_names_urls.py
@@ -0,0 +1,13 @@
+const_names_urls = {
+    'portal_do_paciente': 'portal_do_paciente',
+    'gestao_de_pacientes': 'gestao_de_pacientes',
+    'gestao_de_medicos': 'gestao_de_medicos',
+    'agendar_consulta': 'agendar_consulta',
+    'logout': 'logout',
+    'editar_paciente': 'editar_paciente',
+    'remover_paciente': 'remover_paciente',
+    'editar_medico': 'editar_medico',
+    'remover_medico': 'remover_medico',
+}
+
+    
--- portal_do_paciente/urls.py
+++ portal_do_paciente/urls.py
@@ -1,15 +1,18 @@
 from . import views
 from django.urls import path
 from . import views_medicos
+from . import const_names_urls as name_url
+
+url_name = name_url.const_names_urls
 
 urlpatterns = [
-    path('<int:id>/', views.portal_do_paciente, name='portal_do_paciente'), 
-    path('<int:id>/gestao_de_pacientes/', views.gestao_de_pacientes, name='gestao_de_pacientes'),
-    path('<int:id>/gestao_de_medicos/', views_medicos.gestao_de_medicos, name='gestao_de_medicos'),
-    path('<int:id>', views.agendar_consulta, name='agendar_consulta'),
-    path('logout/', views.logout_view, name='logout'),
-    path('paciente/<int:id_adm>/<int:id_paciente>/editar/', views.editar_paciente, name='editar_paciente'),
-    path('paciente/<int:id>/<int:id_adm>remover/', views.remover_paciente, name='remover_paciente'),
-    path('paciente/<int:id_adm>/<int:id_medico>/EditarMedico', views_medicos.editar_medico, name='editar_medico'),
-    path('paciente/<int:id_adm>/<int:id_medico>/ExcluirMedico/', views_medicos.remover_medico, name='remover_medico'),
+    path('<int:id>/', views.portal_do_paciente, name=url_name['portal_do_paciente']), 
+    path('<int:id>/gestao_de_pacientes/', views.gestao_de_pacientes, name=url_name['gestao_de_pacientes']),
+    path('<int:id>/gestao_de_medicos/', views_medicos.gestao_de_medicos, name=url_name['gestao_de_medicos']),
+    path('<int:id>', views.agendar_consulta, name=url_name['agendar_consulta']),
+    path('logout/', views.logout_view, name=url_name['logout']),
+    path('paciente/<int:id_adm>/<int:id_paciente>/editar/', views.editar_paciente, name=url_name['editar_paciente']),
+    path('paciente/<int:id>/<int:id_adm>remover/', views.remover_paciente, name=url_name['remover_paciente']),
+    path('paciente/<int:id_adm>/<int:id_medico>/EditarMedico', views_medicos.editar_medico, name=url_name['gestao_de_medicos']),
+    path('paciente/<int:id_adm>/<int:id_medico>/ExcluirMedico/', views_medicos.remover_medico, name=url_name['remover_medico']),
 ]
